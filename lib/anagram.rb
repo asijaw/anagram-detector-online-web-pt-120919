b7cfd8092ebcebfa45af6b41c5cc790999dc9df9
Asija Watson <asija.w@yahoo.com>
2020-01-12 21:12:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :keyword
+  def initialize
+    @keyword = keyword
+  end
+  
+  def match(word_list)
+    word_list.each {|word| word.split.sort == @keyword}
+  end
+end
+    
